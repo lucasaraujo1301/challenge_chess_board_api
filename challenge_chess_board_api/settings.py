8c64048df82a292d3219d643f42082c172d01b7c
Lucas Araujo de Medeiros <lucasaraujo1301@gmail.com>
2022-02-25 19:32:54 -0300

    chore: configuring database settings

--- challenge_chess_board_api/settings.py
+++ challenge_chess_board_api/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'challenge_chess_board_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chess_board',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
