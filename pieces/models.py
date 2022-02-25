3a09b8bc9649a53209a03c601d62f831ea5aaa90
Lucas Araujo de Medeiros <lucasaraujo1301@gmail.com>
2022-02-25 18:20:26 -0300

    chore: creating Piece and Color model

--- pieces/models.py
+++ pieces/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+PIECES_CHOICES = (
+    ('queen', 'Queen'), ('king', 'King'), ('tower', 'tower'), ('bishop', 'Bishop'), ('pawn', 'Pawn'),
+    ('knight', 'Knight')
+)
+COLOR_CHOICES = (('white', 'White'), ('black', 'Black'))
+
+
 # Create your models here.
+class Color(models.Model):
+    name = models.CharField(max_length=5, choices=COLOR_CHOICES)
+
+    def __str__(self):
+        return self.name
+
+
+class Piece(models.Model):
+    name = models.CharField(max_length=6, choices=PIECES_CHOICES)
+    color = models.ForeignKey(Color, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'{self.name} - {self.color.name}'
+
+    class Meta:
+        ordering = ['name']
